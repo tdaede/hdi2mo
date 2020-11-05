9ff9c5a78b8e46ea92a8e7a552e4b1f28d48088b
Thomas Daede <thomas@kaban.lan>
2020-11-04 23:53:14 -0800

    stuff

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 extern crate clap;
 use clap::{Arg, Command};
+extern crate byteorder;
 use std::fs::File;
 use std::path::PathBuf;
 use std::io;
 use std::io::prelude::*;
 use std::io::SeekFrom;
+use byteorder::{ByteOrder, LE};
 
 fn main() -> io::Result<()>{
     let m = Command::new("hdi2mo")
@@ -16,32 +18,50 @@ fn main() -> io::Result<()>{
     .get_matches();
     let mut hdi_file = File::open(m.get_one::<PathBuf>("in_file").unwrap())?;
     let mut mo_file = File::create(m.get_one::<PathBuf>("mo_file").unwrap())?;
-    let mut template_file = File::open("/home/thomas/formatted_mo.img")?;
+    let mut template_file = File::open("/home/thomas/sandbox/hdi2mo/formatted_mo.img")?;
     hdi_file.seek(SeekFrom::Current(0x1000))?; // skip 4096 byte header
     let mut fat16_header = [0; 512];
     template_file.read(&mut fat16_header)?;
-    //fat16_header[0x0B] = 0x00; // logical sector size
-    //fat16_header[0x0C] = 0x04;
-    //fat16_header[0x0D] = 0x02; // sectors per cluster
+    let mut hdi_ipl = [0; 512];
+    hdi_file.read(&mut hdi_ipl)?;
+    if (hdi_ipl[0xfe] != 0x55) || (hdi_ipl[0xff] != 0xaa) {
+        panic!("Couldn't find NEC partition magic");
+    }
+    let mut hdi_part_table = [0; 512];
+    hdi_file.read(&mut hdi_part_table)?;
+    hdi_file.seek(SeekFrom::Current(0x8400))?; // skip mbr data and other trash
+    let mut hdi_fat16_header = [0; 512];
+    hdi_file.read(&mut hdi_fat16_header)?;
+
+    let bytes_per_sector = LE::read_u16(&hdi_fat16_header[0xb..0xd]);
+    let sectors_per_cluster = hdi_fat16_header[0xd];
+    let reserved_sectors = LE::read_u16(&hdi_fat16_header[0xe..0x10]);
+    let total_logical_sectors = LE::read_u16(&hdi_fat16_header[0x13..0x15]);
+    let sectors_per_fat = LE::read_u16(&hdi_fat16_header[0x16..0x18]);
+    println!("bytes per sector: {}", bytes_per_sector);
+    println!("sectors per cluster: {}", sectors_per_cluster);
+    println!("reserved sectors: {}", reserved_sectors);
+    println!("total logical sectors: {}", total_logical_sectors);
+    println!("sectors per fat: {}", sectors_per_fat);
+    let bytes_per_cluster = bytes_per_sector * sectors_per_cluster as u16;
+    println!("bytes per cluster: {}", bytes_per_cluster);
+
+    LE::write_u16(&mut fat16_header[0xb..0xd], bytes_per_sector);
+    fat16_header[0x0D] = sectors_per_cluster; // sectors per cluster
     //fat16_header[0x10] = 0x01; // one FAT
-    fat16_header[0x16] = 0x08; // sectors in FAT
-    fat16_header[0x17] = 0x00;
-    fat16_header[0x20] = 0xd1;
-    fat16_header[0x21] = 0x22;
-    fat16_header[0x22] = 0x00;
-    fat16_header[0x23] = 0x00;
+    //fat16_header[0x16] = 0x08; // sectors in FAT
+    //fat16_header[0x17] = 0x00;
+    LE::write_u16(&mut fat16_header[0x16..0x18], sectors_per_fat);
+    //fat16_header[0x20] = 0xd1;
+    //fat16_header[0x21] = 0x22;
+    //fat16_header[0x22] = 0x00;
+    //fat16_header[0x23] = 0x00;
+    LE::write_u32(&mut fat16_header[0x20..0x24], total_logical_sectors as u32);
     mo_file.write(&fat16_header)?;
-    //mo_file.write(&[0;512])?; // extra padding for 1st 1024 byte sector
-    hdi_file.seek(SeekFrom::Current(0x8800))?; // skip mbr data and other trash
-    hdi_file.seek(SeekFrom::Current(0x400))?; // skip fat16 header
     // copy fat tables verbatim
     let mut fat_data = [0; 0x2000];
-  hdi_file.read(&mut fat_data)?;
-  fat_data[0] = 0xF0;
+    hdi_file.read(&mut fat_data)?;
     mo_file.write(&fat_data)?;
-    // now we need to write more garbage fat data
-    //mo_file.write(&[0; 0x32600])?;
-    //mo_file.write(&[0; 0x6f200])?;
     // finally write directory and file data
     let mut clusters = vec![];
     hdi_file.read_to_end(&mut clusters)?;
