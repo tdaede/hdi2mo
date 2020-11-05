3b036d42aad2873a203387db33005f6b942836c3
Thomas Daede <thomas@kaban.lan>
2020-11-05 00:54:29 -0800

    1024

--- src/main.rs
+++ src/main.rs
@@ -21,46 +21,58 @@ fn main() -> io::Result<()>{
     let mut template_file = File::open("/home/thomas/sandbox/hdi2mo/formatted_mo.img")?;
     hdi_file.seek(SeekFrom::Current(0x1000))?; // skip 4096 byte header
     let mut fat16_header = [0; 512];
-    template_file.read(&mut fat16_header)?;
+    template_file.read_exact(&mut fat16_header)?;
     let mut hdi_ipl = [0; 512];
-    hdi_file.read(&mut hdi_ipl)?;
+    hdi_file.read_exact(&mut hdi_ipl)?;
     if (hdi_ipl[0xfe] != 0x55) || (hdi_ipl[0xff] != 0xaa) {
         panic!("Couldn't find NEC partition magic");
     }
     let mut hdi_part_table = [0; 512];
-    hdi_file.read(&mut hdi_part_table)?;
+    hdi_file.read_exact(&mut hdi_part_table)?;
     hdi_file.seek(SeekFrom::Current(0x8400))?; // skip mbr data and other trash
     let mut hdi_fat16_header = [0; 512];
-    hdi_file.read(&mut hdi_fat16_header)?;
+    hdi_file.read_exact(&mut hdi_fat16_header)?;
 
-    let bytes_per_sector = LE::read_u16(&hdi_fat16_header[0xb..0xd]);
-    let sectors_per_cluster = hdi_fat16_header[0xd];
+    let bytes_per_sector = LE::read_u16(&hdi_fat16_header[0xb..0xd]) as u32;
+    let sectors_per_cluster = hdi_fat16_header[0xd] as u32;
     let reserved_sectors = LE::read_u16(&hdi_fat16_header[0xe..0x10]);
-    let total_logical_sectors = LE::read_u16(&hdi_fat16_header[0x13..0x15]);
-    let sectors_per_fat = LE::read_u16(&hdi_fat16_header[0x16..0x18]);
+    let total_logical_sectors = LE::read_u16(&hdi_fat16_header[0x13..0x15]) as u32;
+    let sectors_per_fat = LE::read_u16(&hdi_fat16_header[0x16..0x18]) as u32;
+    let num_fats = 2;
     println!("bytes per sector: {}", bytes_per_sector);
     println!("sectors per cluster: {}", sectors_per_cluster);
     println!("reserved sectors: {}", reserved_sectors);
+    assert_eq!(reserved_sectors, 1);
     println!("total logical sectors: {}", total_logical_sectors);
     println!("sectors per fat: {}", sectors_per_fat);
-    let bytes_per_cluster = bytes_per_sector * sectors_per_cluster as u16;
+    let bytes_per_cluster = bytes_per_sector * sectors_per_cluster;
     println!("bytes per cluster: {}", bytes_per_cluster);
 
-    LE::write_u16(&mut fat16_header[0xb..0xd], bytes_per_sector);
-    fat16_header[0x0D] = sectors_per_cluster; // sectors per cluster
+    let mo_bytes_per_sector = 1024;
+    let mo_total_logical_sectors = total_logical_sectors * bytes_per_sector / mo_bytes_per_sector;
+    let mo_sectors_per_fat = (sectors_per_fat * bytes_per_sector / mo_bytes_per_sector) as u16;
+
+    LE::write_u16(&mut fat16_header[0xb..0xd], mo_bytes_per_sector as u16);
+    fat16_header[0x0D] = (bytes_per_cluster / mo_bytes_per_sector) as u8; // sectors per cluster
     //fat16_header[0x10] = 0x01; // one FAT
     //fat16_header[0x16] = 0x08; // sectors in FAT
     //fat16_header[0x17] = 0x00;
-    LE::write_u16(&mut fat16_header[0x16..0x18], sectors_per_fat);
+    LE::write_u16(&mut fat16_header[0x16..0x18], mo_sectors_per_fat);
     //fat16_header[0x20] = 0xd1;
     //fat16_header[0x21] = 0x22;
     //fat16_header[0x22] = 0x00;
     //fat16_header[0x23] = 0x00;
-    LE::write_u32(&mut fat16_header[0x20..0x24], total_logical_sectors as u32);
+    LE::write_u32(&mut fat16_header[0x20..0x24], mo_total_logical_sectors as u32);
     mo_file.write(&fat16_header)?;
+    // skip past first reserved sector
+    hdi_file.seek(SeekFrom::Current(reserved_sectors as i64 * bytes_per_sector as i64 - 512))?;
+    // write reserved sector if necessary
+    let reserved_bytes = vec![0; reserved_sectors as usize * mo_bytes_per_sector as usize - 512];
+    mo_file.write(&reserved_bytes)?;
     // copy fat tables verbatim
-    let mut fat_data = [0; 0x2000];
-    hdi_file.read(&mut fat_data)?;
+    let mut fat_data = vec![0; num_fats * sectors_per_fat as usize * bytes_per_sector as usize];
+    hdi_file.read_exact(&mut fat_data)?;
+    //fat_data[0] = 0xF0;
     mo_file.write(&fat_data)?;
     // finally write directory and file data
     let mut clusters = vec![];
