93dcc8cf15492ad273ffbfe25acc5581320c48a1
Thomas Daede <daede003@umn.edu>
2023-10-22 21:26:04 -0700

    use binrw for bpb parsing

--- src/main.rs
+++ src/main.rs
@@ -52,6 +52,20 @@ struct PC98PartitionTable {
     partitions: [PC98Partition; 16],
 }
 
+#[binrw]
+#[brw(little)]
+#[derive(Debug, Copy, Clone)]
+struct BIOSParameterBlock {
+    bytes_per_sector: u16,
+    sectors_per_cluster: u8,
+    reserved_sectors: u16,
+    num_fats: u8,
+    max_root_dirents: u16,
+    total_logical_sectors: u16,
+    media_id: u8,
+    sectors_per_fat: u16,
+}
+
 fn main() -> io::Result<()>{
     let m = Command::new("hdi2mo")
         .about("Convert hdi image file to a MO-compatible FAT16 file system")
@@ -72,6 +86,7 @@ fn main() -> io::Result<()>{
     if (hdi_ipl[0xfe] != 0x55) || (hdi_ipl[0xff] != 0xaa) {
         panic!("Couldn't find NEC partition magic");
     }
+    println!("hdd sector size: {}", hdi_header.bytes_per_sector);
     let hdi_part_table = PC98PartitionTable::read(&mut hdi_file).unwrap();
     for p in hdi_part_table.partitions {
         if p.mid != 0 {
@@ -79,39 +94,35 @@ fn main() -> io::Result<()>{
         }
     }
     let p = hdi_part_table.partitions[0];
-    hdi_file.seek(SeekFrom::Start(hdi_header.header_size as u64
+    let p_start_offset = hdi_header.header_size as u64
                                   + ((p.scyl as u64 * hdi_header.heads as u64 + p.shd as u64)
                                      * hdi_header.sectors as u64 + p.ssect as u64)
-                                  * hdi_header.bytes_per_sector as u64
-    ))?;
+                                  * hdi_header.bytes_per_sector as u64;
+    hdi_file.seek(SeekFrom::Start(p_start_offset))?;
     let mut hdi_fat16_header = [0; 512];
     hdi_file.read_exact(&mut hdi_fat16_header)?;
+    hdi_file.seek(SeekFrom::Start(p_start_offset + 0x0b))?;
 
-    let bytes_per_sector = LE::read_u16(&hdi_fat16_header[0xb..0xd]) as u32;
-    let sectors_per_cluster = hdi_fat16_header[0xd] as u32;
-    let reserved_sectors = LE::read_u16(&hdi_fat16_header[0xe..0x10]);
-    let total_logical_sectors = LE::read_u16(&hdi_fat16_header[0x13..0x15]) as u32;
-    let sectors_per_fat = LE::read_u16(&hdi_fat16_header[0x16..0x18]) as u32;
-    let max_root_dirents = LE::read_u16(&hdi_fat16_header[0x11..0x13]);
-    let num_fats = 2;
-    println!("bytes per sector: {}", bytes_per_sector);
-    println!("sectors per cluster: {}", sectors_per_cluster);
-    println!("reserved sectors: {}", reserved_sectors);
-    assert_eq!(reserved_sectors, 1);
-    println!("total logical sectors: {}", total_logical_sectors);
-    println!("sectors per fat: {}", sectors_per_fat);
-    let bytes_per_cluster = bytes_per_sector * sectors_per_cluster;
+    let bpb = BIOSParameterBlock::read(&mut hdi_file).unwrap();
+
+    println!("bytes per sector: {}", bpb.bytes_per_sector);
+    println!("sectors per cluster: {}", bpb.sectors_per_cluster);
+    println!("reserved sectors: {}", bpb.reserved_sectors);
+    assert_eq!(bpb.reserved_sectors, 1);
+    println!("total logical sectors: {}", bpb.total_logical_sectors);
+    println!("sectors per fat: {}", bpb.sectors_per_fat);
+    let bytes_per_cluster = bpb.bytes_per_sector as usize* bpb.sectors_per_cluster as usize;
     println!("bytes per cluster: {}", bytes_per_cluster);
-    println!("max root dirents: {}", max_root_dirents);
+    println!("max root dirents: {}", bpb.max_root_dirents);
 
-    let mo_bytes_per_sector = bytes_per_sector;
-    let mo_total_logical_sectors = total_logical_sectors * bytes_per_sector / mo_bytes_per_sector;
-    let mo_sectors_per_fat = (sectors_per_fat * bytes_per_sector / mo_bytes_per_sector) as u16;
+    let mo_bytes_per_sector = bpb.bytes_per_sector;
+    let mo_total_logical_sectors = bpb.total_logical_sectors as u64 * bpb.bytes_per_sector as u64 / mo_bytes_per_sector as u64;
+    let mo_sectors_per_fat = (bpb.sectors_per_fat * bpb.bytes_per_sector / mo_bytes_per_sector) as u16;
 
     LE::write_u16(&mut fat16_header[0xb..0xd], mo_bytes_per_sector as u16);
-    fat16_header[0x0D] = (bytes_per_cluster / mo_bytes_per_sector) as u8; // sectors per cluster
+    fat16_header[0x0D] = (bytes_per_cluster / mo_bytes_per_sector as usize) as u8; // sectors per cluster
     //fat16_header[0x10] = 0x01; // one FAT
-    LE::write_u16(&mut fat16_header[0x11..0x13], max_root_dirents);
+    LE::write_u16(&mut fat16_header[0x11..0x13], bpb.max_root_dirents);
     //fat16_header[0x16] = 0x08; // sectors in FAT
     //fat16_header[0x17] = 0x00;
     LE::write_u16(&mut fat16_header[0x16..0x18], mo_sectors_per_fat);
@@ -122,12 +133,12 @@ fn main() -> io::Result<()>{
     LE::write_u32(&mut fat16_header[0x20..0x24], mo_total_logical_sectors as u32);
     mo_file.write(&fat16_header)?;
     // skip past first reserved sector
-    hdi_file.seek(SeekFrom::Current(reserved_sectors as i64 * bytes_per_sector as i64 - 512))?;
+    hdi_file.seek(SeekFrom::Start(bpb.reserved_sectors as u64 * bpb.bytes_per_sector as u64 + p_start_offset))?;
     // write reserved sector if necessary
-    let reserved_bytes = vec![0; reserved_sectors as usize * mo_bytes_per_sector as usize - 512];
+    let reserved_bytes = vec![0; bpb.reserved_sectors as usize * mo_bytes_per_sector as usize - 512];
     mo_file.write(&reserved_bytes)?;
     // copy fat tables verbatim
-    let mut fat_data = vec![0; num_fats * sectors_per_fat as usize * bytes_per_sector as usize];
+    let mut fat_data = vec![0; bpb.num_fats as usize * bpb.sectors_per_fat as usize * bpb.bytes_per_sector as usize];
     hdi_file.read_exact(&mut fat_data)?;
     //fat_data[0] = 0xF0;
     mo_file.write(&fat_data)?;
